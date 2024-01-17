0bc7b3b51ce608d0c06ff635592900f4ca258a6c
Sam <58117545+Sam-Malta@users.noreply.github.com>
2024-01-17 17:26:03 -0500

    Fix: Rewrote the broken stuff

--- src/main/java/frc/robot/commands/SwerveHoming.java
+++ src/main/java/frc/robot/commands/SwerveHoming.java
@@ -2,10 +2,9 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 
-public class SwerveHoming extends CommandBase {
+public class SwerveHoming extends Command {
   
   private SwerveDriveSubsystem swerveSubsystem;
 
