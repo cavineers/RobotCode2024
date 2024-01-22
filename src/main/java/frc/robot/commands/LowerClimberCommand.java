bef51bcbeb1b56a72afac34893dec9d4266c6079
ashley064 <115119764+ashley064@users.noreply.github.com>
2024-01-23 08:51:04 +1100

    Work on the RiseClimberCommand

--- src/main/java/frc/robot/commands/LowerClimberCommand.java
+++ src/main/java/frc/robot/commands/LowerClimberCommand.java
@@ -1,5 +1,11 @@
 package frc.robot.commands;
 
-public class LowerClimberCommand {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.subsystems.LeftClimber;
+import frc.robot.subsystems.RightClimber;
+
+public class LowerClimberCommand extends CommandBase {
     
 }
--- src/main/java/frc/robot/commands/RiseClimberCommand.java
+++ src/main/java/frc/robot/commands/RiseClimberCommand.java
@@ -1,5 +1,16 @@
 package frc.robot.commands;
 
-public class RiseClimberCommand {
-    //Accept two parameters - climberSide (string) and riseBot (boolean)
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.subsystems.LeftClimber;
+import frc.robot.subsystems.RightClimber;
+
+public class RiseClimberCommand extends CommandBases{
+
+    private boolean complete;
+
+    public RiseClimberCommand() {
+        this.addRequirements(Robot.leftClimber);
+    }
 }
