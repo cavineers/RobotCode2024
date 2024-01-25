26827322006d7367bbad7bbe6b5105764c9a3834
Charles-M-Hecker <91638168+Charles-M-Hecker@users.noreply.github.com>
2024-01-25 14:21:16 -0500

    clarified state and variable names

--- src/main/java/frc/robot/subsystems/ArmBase.java
+++ src/main/java/frc/robot/subsystems/ArmBase.java
@@ -10,57 +10,68 @@ import frc.robot.Constants;
 
 public class ArmBase extends SubsystemBase {
 
-    public void periodic() {}
-
-     public enum MotorState {
+    
+    public enum BaseMotorState {
         ON,
         OFF,
         REVERSED
     }
-
+    
     // Motor Initialization
     public CANSparkMax baseMotor = new CANSparkMax(Constants.ArmBase.BaseMotor, MotorType.kBrushless);
     
     // Starts motors in their off state
-    public MotorState baseMotorState = MotorState.OFF;
-
+    public BaseMotorState baseMotorState = BaseMotorState.OFF;
+    
     // Motor sparkmax settings
     public ArmBase() {
         this.baseMotor.setIdleMode(IdleMode.kBrake);
-
+        
         this.baseMotor.setSmartCurrentLimit(51);
-       
     }
     
-    public void setBaseMotorState(MotorState state) {
+    public void setBaseMotorState(BaseMotorState state) {
         // set the current state
         this.baseMotorState = state;
         
         // set motor state
         switch (state) {
             case ON:
-                // On
-                this.baseMotor.set(Constants.ArmBase.BaseMotorSpeedForwards);
-                break;
+            // On
+            this.baseMotor.set(Constants.ArmBase.BaseMotorSpeedForwards);
+            break;
+            
             case OFF:
-                // Off
-                this.baseMotor.set(0);
-                
-                break;
+            // Off
+            this.baseMotor.set(0);
+            break;
+            
             case REVERSED:
-                // Reversed
+            // Reversed
                 this.baseMotor.set(Constants.ArmBase.BaseMotorSpeedBackwards);
                 break;
-            default:
-                this.setBaseMotorState(MotorState.OFF);
-        }
-    }
 
+                default:
+                this.setBaseMotorState(BaseMotorState.OFF);
+            }
+        }
+        
     public double getBaseMotorPosition() {
         return this.baseMotor.getEncoder().getPosition();
     }
 
+    public double getBaseMotorSpeed() {
+        return this.baseMotor.get();
+    }
+    
+    public BaseMotorState getBaseMotorState() {
+        return this.baseMotorState;
+    }
+    
     public void setBaseMotorPosition(double position) {
         this.baseMotor.getEncoder().setPosition(position);
     }
+    
+    public void periodic() {}
+    
 }
--- src/main/java/frc/robot/subsystems/ArmPivot.java
+++ src/main/java/frc/robot/subsystems/ArmPivot.java
@@ -7,32 +7,32 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.subsystems.ArmBase.BaseMotorState;
 
 public class ArmPivot extends SubsystemBase {
 
-    public void periodic() {}
-
-     public enum MotorState {
+    
+    public enum PivotMotorState {
         ON,
         OFF,
         REVERSED
     }
-
+    
     // Motor Initialization
     public CANSparkMax pivotMotor = new CANSparkMax(Constants.ArmPivot.PivotMotor, MotorType.kBrushless);
     
     // Starts motors in their off state
-    public MotorState pivotMotorState = MotorState.OFF;
-
+    public PivotMotorState pivotMotorState = PivotMotorState.OFF;
+    
     // Motor sparkmax settings
     public ArmPivot() {
         this.pivotMotor.setIdleMode(IdleMode.kBrake);
-
+        
         this.pivotMotor.setSmartCurrentLimit(51);
-       
+        
     }
     
-    public void setPivotMotorState(MotorState state) {
+    public void setPivotMotorState(PivotMotorState state) {
         // set the current state
         this.pivotMotorState = state;
         
@@ -42,17 +42,19 @@ public class ArmPivot extends SubsystemBase {
                 // On
                 this.pivotMotor.set(Constants.ArmPivot.PivotMotorSpeedForwards);
                 break;
+
             case OFF:
                 // Off
                 this.pivotMotor.set(0);
-                
                 break;
+
             case REVERSED:
                 // Reversed
                 this.pivotMotor.set(Constants.ArmPivot.PivotMotorSpeedBackwards);
                 break;
+
             default:
-                this.setPivotMotorState(MotorState.OFF);
+                this.setPivotMotorState(PivotMotorState.OFF);
         }
     }
 
@@ -60,7 +62,17 @@ public class ArmPivot extends SubsystemBase {
         return this.pivotMotor.getEncoder().getPosition();
     }
 
+    public double getPivotMotorSpeed() {
+        return this.pivotMotor.get();
+    }
+
+    public PivotMotorState getPivotMotorState() {
+        return this.pivotMotorState;
+    }
+    
     public void setPivotMotorPosition(double position) {
         this.pivotMotor.getEncoder().setPosition(position);
     }
+    public void periodic() {}
+
 }
