4019c85197f0c594f0ce3814327db11739632879
Bryan Ha <93681387+HaBr24@users.noreply.github.com>
2024-01-28 23:59:18 -0500

    got help from the goat charles, did stuff for command and robot container, it was super lit yo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,9 @@ import frc.robot.Constants.OIConstants;
 import frc.robot.commands.SwerveHoming;
 import frc.robot.commands.Arm.ArmPreset;
 import frc.robot.Robot;
+import frc.robot.subsystems.ArmBase;
+import frc.robot.commands.Arm.Gantry.GantryManualLower;
+import frc.robot.commards.Arm.Gantry.GantryManualRaise;
 
 
 public class RobotContainer {
@@ -26,8 +29,16 @@ public class RobotContainer {
     public JoystickButton l_bump;
 
     public SwerveHoming swerveHomingCommand;
+   
+    private final ArmBase armBase;
+    
+    //Commands
+    public Command gantryManualRaise;
+    public Command gantryManualLower;
 
     public RobotContainer() {
+
+        armBase = new ArmBase();
 
         groundPreset = new ArmPreset(Constants.ArmBase.GroundPositionRotations, Constants.ArmPivot.PivotMotorGroundRotations);
 
@@ -38,7 +49,9 @@ public class RobotContainer {
         driverJoystick = new Joystick(OIConstants.kDriverJoystickPort);
         button = new JoystickButton(driverJoystick, 4);
         l_bump = new JoystickButton(driverJoystick, 5);
-        
+    
+        gantryManualRaise = new GantryManualRaise(armBase);
+        gantryManualLower = new GantryManualLower(armBase);
 
         swerveSubsystem.setDefaultCommand(new SwerveCommand(
             swerveSubsystem,
