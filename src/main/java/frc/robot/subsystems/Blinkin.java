d0c6fea7c404e9ebd57ba5385da2818b2b012b0d
Logan Bechtel <bechtello25@spaldinghs.org>
2024-02-08 13:28:47 -0500

    Feat: Led lights in integration

---
+++ src/main/java/frc/robot/subsystems/Blinkin.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Blinkin extends SubsystemBase {
+	
+	Spark blinkin;
+
+	public Blinkin() {
+		blinkin = new Spark(0);
+	}
+
+	public void lightsRainbow() {
+		blinkin.set(-0.99);
+	}
+
+	public void lightsFire() {
+		blinkin.set(-0.59);
+	}
+
+	public void lightsChase() {
+		blinkin.set(0.01);
+	}
+
+	public void lightsOcean() {
+		blinkin.set(-0.41);
+	}
+}
