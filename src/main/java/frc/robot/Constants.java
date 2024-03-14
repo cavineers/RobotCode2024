b74b836b27ce0a9dfb8d98768dd72c6ab8e04e8b
Logan Bechtel <bechtello25@spaldinghs.org>
2024-03-14 11:41:41 -0400

    Fix: Dio switch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -59,7 +59,7 @@ public final class Constants {
         public static final int ArmBoreEncoder = 0;
 
         public static final int GantryLowerLimitSwitch = 1;
-        public static final int GantryHigherLimitSwitch = 2;
+        public static final int GantryHigherLimitSwitch = 8;
 
         public static final int LeftClimberLimitSwitch = 6; 
         public static final int RightClimberLimitSwitch = 9;
