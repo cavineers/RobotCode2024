bc71cb1b23b256cb18f5ec5ed205b2997f4b5e42
Robert Snell <91638194+roberts1y@users.noreply.github.com>
2024-03-28 15:33:24 -0400

    update auto

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -138,7 +138,7 @@ public final class Constants {
     
         public static final double DistancePerRotation = 1;
 
-        public static final double PivotSetpointTolerance = 0.02;
+        public static final double PivotSetpointTolerance = 0.025;
     
         public static final double GroundPickupRotations = 0.353 - 0.007;
         public static final double RestRotations = 0.337;
